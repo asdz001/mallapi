959d8de4e98e1c80fcb84fc245748c08ae358853
sunguk Choi <asdz3539@naver.com>
2025-06-17 16:10:17 +0900

    🔁카테고리 등록 (수정/삭제 포함)

--- dictionary/models.py
+++ dictionary/models.py
@@ -34,40 +34,40 @@ class ForbiddenWord(models.Model):
 
 
 #카테고리
-# ✅ 대분류
+# ✅ 성별
 class CategoryLevel1(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="표준 대분류")
+    name = models.CharField(max_length=100, unique=True, verbose_name="표준 성별")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "대분류카테고리"
-        verbose_name_plural = "1. 대분류"
+        verbose_name = "성별"
+        verbose_name_plural = "1. 성별"
 
 
 class CategoryLevel1Alias(models.Model):
-    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 대분류")
+    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 성별")
     category = models.ForeignKey(CategoryLevel1, on_delete=models.CASCADE, related_name="aliases")
 
     def __str__(self):
         return f"{self.alias} → {self.category.name}"
 
 
-# ✅ 중분류
+# ✅ 대분류
 class CategoryLevel2(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="표준 중분류")
+    name = models.CharField(max_length=100, unique=True, verbose_name="표준 대분류")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "중분류카테고리"
-        verbose_name_plural = "2. 중분류"
+        verbose_name = "대분류"
+        verbose_name_plural = "2. 대분류"
 
 
 class CategoryLevel2Alias(models.Model):
-    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 중분류")
+    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 대분류")
     category = models.ForeignKey(CategoryLevel2, on_delete=models.CASCADE, related_name="aliases")
 
     def __str__(self):
@@ -76,20 +76,20 @@ class CategoryLevel2Alias(models.Model):
 
 
 
-# ✅ 소분류
+# ✅ 중분류
 class CategoryLevel3(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="표준 소분류")
+    name = models.CharField(max_length=100, unique=True, verbose_name="표준 중분류")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "소분류"
-        verbose_name_plural = "3. 소분류"
+        verbose_name = "중분류"
+        verbose_name_plural = "3. 중분류"
 
 
 class CategoryLevel3Alias(models.Model):
-    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 소분류")
+    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 중분류")
     category = models.ForeignKey(CategoryLevel3, on_delete=models.CASCADE, related_name="aliases")
 
     def __str__(self):
@@ -99,19 +99,19 @@ class CategoryLevel3Alias(models.Model):
 
 
 
-# ✅ 소소분류
+# ✅ 소분류
 class CategoryLevel4(models.Model):
-    name = models.CharField(max_length=100, unique=True, verbose_name="표준 소소분류")
+    name = models.CharField(max_length=100, unique=True, verbose_name="표준 소분류")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "소소분류"
-        verbose_name_plural = "4. 소소분류"    
+        verbose_name = "소분류"
+        verbose_name_plural = "4. 소분류"    
 
 class CategoryLevel4Alias(models.Model):
-    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 소소분류")
+    alias = models.CharField(max_length=100, unique=True, verbose_name="치환 소분류")
     category = models.ForeignKey(CategoryLevel4, on_delete=models.CASCADE, related_name="aliases")
 
     def __str__(self):
