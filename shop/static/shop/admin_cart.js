3a14833500ebc66d5c49f8ace3175fb43c3ea666
sunguk Choi <asdz3539@naver.com>
2025-06-20 12:13:44 +0900

    🔁 정적파일 전송

--- shop/static/shop/admin_cart.js
+++ shop/static/shop/admin_cart.js
@@ -181,6 +181,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+// 장바구니 옵션 저장 함수
 function saveCart(cartId) {
   const container = document.getElementById(`cart-${cartId}`);
   const inputs = container.querySelectorAll(".cart-qty-input");
